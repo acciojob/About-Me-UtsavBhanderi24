42253f2bbd084b86cb8aa99bb30b187946c270b6
Utsav Bhanderi <utsavbhanderi09@gmail.com>
2025-06-11 09:25:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 // Your code here.
+<!-- Bootstrap JS (optional, only if you want interactive components) -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
